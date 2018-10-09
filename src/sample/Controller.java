f35ab627a8682d1f092688408decd10706a236af
Michal Banas <misobanas62@gmail.com>
2018-10-09 21:00:07 +0200

    vacsi commit - po kliku na tlacitko sa zavola funkcia handle(), ktora natiahne   text z textfieldu a posle ho do nizsie popisanej triedy - pridana trieda XMLOutputter 	- riesi vytvaranie xml pomocou elementov a atributov 	- zatial len copy paste, treba to spravit dynamicky cez nejaky cyklus 	- zoberie argumenty - text z UI a capne to do XML 	- XML presne tvorene podla predlohy 	- <el></el> sa tvori pomocou Element 	- atribut elementu pomocou Attribute (zakomentovane)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class Controller {
+import static sample.XMLOutputter.spracuj;
+
+public class Controller implements EventHandler<ActionEvent> {
+
+    public TextField nameTextField;
+    Button nameButton = new Button();
+    TextArea ta = new TextArea();
+    String name = "";
+
+    @Override
+    public void handle(ActionEvent event) {
+
+        // TAKTO VIEME VYTIAHNUT TEXT Z TEXTFIELDU, TOTO POTREBUJEME DOSTAT
+        // DO TRIEDY XMLOutputter A Z TYCHTO DAT SPRAVIT XML
+
+        name=nameTextField.getText();
+        spracuj(name);
+    }
 }
---
+++ src/sample/XMLOutputter.java
@@ -0,0 +1,88 @@
+package sample;
+
+import org.jdom2.*;
+import org.jdom2.output.Format;
+
+public class XMLOutputter {
+
+    public static void spracuj(String nameTextfield){
+
+        // create the jdom
+        Document jdomDoc = new Document();
+        // create root element
+        Element rootElement = new Element("car-rent");
+        jdomDoc.setRootElement(rootElement);
+
+        // add child
+        Element orderPerson = new Element("orderperson");
+
+            Element person = new Element("person");
+            orderPerson.addContent(person);
+
+                Element name = new Element("name");
+                name.addContent(nameTextfield);
+                person.addContent(name);
+
+                Element date = new Element("date-of-birth");
+                date.addContent("09-12-1993");
+                person.addContent(date);
+
+                Element id = new Element("id");
+                id.addContent("EC123056");
+                person.addContent(id);
+
+                Element email = new Element("email");
+                email.addContent("john@smith.com");
+                person.addContent(email);
+
+                Element phone = new Element("phone-number");
+                phone.addContent("+421911099339");
+                person.addContent(phone);
+
+        rootElement.addContent(orderPerson);
+
+        Element passengers = new Element("passengers");
+
+            Element passPerson = new Element("person");
+            passengers.addContent(passPerson);
+
+                Element passName = new Element("name");
+                passName.addContent("John Smith");
+                passPerson.addContent(passName);
+
+                Element passDate = new Element("date-of-birth");
+                passDate.addContent("09-12-1993");
+                passPerson.addContent(passDate);
+
+                Element passId = new Element("id");
+                passId.addContent("EC123056");
+                passPerson.addContent(passId);
+
+                Element passEmail = new Element("email");
+                passEmail.addContent("john@smith.com");
+                passPerson.addContent(passEmail);
+
+                Element passPhone = new Element("phone-number");
+                passPhone.addContent("+421911099339");
+                passPerson.addContent(passPhone);
+
+        rootElement.addContent(passengers);
+
+        //************************************
+        // DOROBIT ZVYSOK ALE ROZUMNE
+        //************************************
+
+
+        // add attributes
+        //Attribute nameAttr = new Attribute("name", "John Smith");
+        //orderPerson.setAttribute(nameAttr);
+
+        // Output as XML
+        // create XMLOutputter
+        org.jdom2.output.XMLOutputter xml = new org.jdom2.output.XMLOutputter();
+        // we want to format the xml. This is used only for demonstration. pretty formatting adds extra spaces and is generally not required.
+        xml.setFormat(Format.getPrettyFormat());
+        System.out.println(xml.outputString(jdomDoc));
+
+    }
+}
