737c031aa2432ad9129f1c535c1945a4155f86dc
Tadeas Bronis <tadeasbronis@Tadeass-MacBook-Pro.local>
2018-10-10 19:20:54 +0200

    Add validation UI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,24 @@
 package sample;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-
+import javafx.stage.Stage;
 import java.util.Date;
 
 import static sample.XMLOutputter.spracuj;
+import static sample.XMLValidator.validate;
 
 public class Controller implements EventHandler<ActionEvent> {
 
-    public TextField nameTextField, evidenceNumberTextField, emailTextField, telephoneTextField;
+    public TextField nameTextField, evidenceNumberTextField, emailTextField, telephoneTextField, xmlTextField, xsdTextField;
     public Date dateTextField;
+    public Label label;
     Button nameButton = new Button();
     TextArea ta = new TextArea();
     String name = "";
@@ -36,5 +42,28 @@ public class Controller implements EventHandler<ActionEvent> {
         customer.setTelephoneNumber(telephoneTextField.getText());
 
         spracuj(customer);
+    }
+
+    public void showValidateUI(ActionEvent event) throws Exception{
+
+        Stage primaryStage = new Stage();
+
+        Parent root = FXMLLoader.load(getClass().getResource("validation.fxml"));
+        primaryStage.setTitle("Validovanie");
+        primaryStage.setScene(new Scene(root, 800, 600));
+
+        primaryStage.show();
+
+    }
+
+    public void validuj() {
+
+        String xml = xmlTextField.getText();
+        String xsd = xsdTextField.getText();
+
+        label.setText(validate(xml,xsd));
+
+        System.out.println(validate(xml,xsd));
+
     }
 }
